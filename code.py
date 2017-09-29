5ee1bbcf788e59f22df5a5f3d39342cd1774c558
mmenschig <marian.menschig@googlemail.com>
2017-09-28 23:00:52 -0700

    update code :lollipop:

--- code.py
+++ code.py
@@ -1,31 +1,39 @@
 #!/home/klaus/Documents/Projects/Cologne-Weather/env/bin/ python
 import requests
+import os
+# from mapping import vapor_pressure as vp
 
-#BASE_URL = 'http://api.wunderground.com/api/403665e38bc0904f/geolookup/conditions/forecast/q/#Germany/Cologne.json'
-BASE_URL = 'http://api.wunderground.com/api/f9d3ddb9a811ec37/geolookup/conditions/forecast/q/Germany/Cologne.json'
+BASE_URL = 'http://api.wunderground.com/api/403665e38bc0904f/geolookup/conditions/forecast/q/Germany/Cologne.json'
 
 r = requests.get(BASE_URL)
 
-print r.status_code
+
+def writeToLog(row):
+    """ 
+    Writes a single row into the specified log file
+    @param row {list} - the row to be written
+    """
+    cwd = os.getcwd()
+    with open(cwd + "/log.txt", "a") as outfile:
+        outfile.write('\t'.join(row) + '\n')
+
 
 if r.status_code == 200:
     data = r.json()
-    print data # THE WHOLE RESPONSE
-    # print data["current_observation"]
-    # print "City is: " + data["current_observation"]["observation_location"]["city"]
-    # print "Dewpoint (f) is: " + str(data["current_observation"]["dewpoint_f"])
+    response = data["current_observation"]
 
-    dewF = str(data["current_observation"]["dewpoint_f"])
-    dewC = str(data["current_observation"]["dewpoint_c"])
-    relH = str(data["current_observation"]["relative_humidity"])
-    prsI = data["current_observation"]["pressure_in"]
-    tmpF = str(data["current_observation"]["temp_f"])
-    tmpC = str(data["current_observation"]["temp_c"])
-    obsL = data["current_observation"]["display_location"]["city"]
-    lt = data["current_observation"]["local_time_rfc822"]
+    # Variable declaration
+    dewF = str(response["dewpoint_f"])
+    dewC = str(response["dewpoint_c"])
+    relH = str(response["relative_humidity"])
+    prsI = str(response["pressure_in"])
+    tmpF = str(response["temp_f"])
+    tmpC = str(response["temp_c"])
+    obsL = str(response["display_location"]["city"])
+    lt = str(response["local_time_rfc822"])
 
-# Calculation of vapor pressure at given temperature
-
+    # Calculation of vapor pressure at given temperature
+    # @mmenschig - I don't know what you're doing here, maybe create a dictionary?
     a0 = 6.107799961
     a1 = 4.436518521E-01
     a2 = 1.428945805E-02
@@ -34,33 +42,25 @@ if r.status_code == 200:
     a5 = 2.034080948E-08
     a6 = 6.136820929E-11
 
-    tmp = (float(tmpF)-32)*5/9
 
-#    print tmp, tmpF, data["current_observation"]["temp_c"]
+    tmp = ((float(tmpF) - 32) * 5 / 9)
 
-    vapprs = str(round(a0+tmp*(a1+tmp*(a2+tmp*(a3+tmp*(a4+tmp*(a5+tmp*a6))))),3))
+    vapprs = str(round(a0 + tmp * (a1 + tmp * (a2 + tmp * (a3 + tmp * (a4 + tmp * (a5 + tmp * a6))))),3))
     vapprs='{:6}'.format(vapprs)
 
-    tmp = (float(dewF)-32)*5/9
+    tmp = ((float(dewF) - 32) * 5 / 9)
 
-    vapdwp = str(round(a0+tmp*(a1+tmp*(a2+tmp*(a3+tmp*(a4+tmp*(a5+tmp*a6))))),3))
+    vapdwp = str(round(a0 + tmp * (a1 + tmp * (a2 + tmp * (a3 + tmp * (a4 + tmp * (a5 + tmp * a6))))),3))
     vapdwp='{:6}'.format(vapdwp)
 
-    # Let's optimize this routine
 
     relH='{:>4}'.format(relH)
+    
+    row = [obsL, lt, dewF, dewC, relH, prsI, tmpF, tmpC, vapprs, vapdwp]
 
-    with open('/home/klaus/Documents/Projects/cologneWeather/log.txt', 'a') as infile:
-        infile.write(obsL + "   " + lt + "   " + dewF + "   " + dewC + "   " + relH + "   " + prsI + "   " + tmpF)
-        infile.write("   " + tmpC + "   " + vapprs + "   " + vapdwp)
-        infile.write('\n')
+    writeToLog(row)
+
 else:
     print "No response returned"
 
-# 1.) use terminal to `cd` into project dir
-# 2.) run `source env/bin/activate` to activate virtual environment
-# 3.) an `(env)` should appear in CLI on left side
-# 4.) run `pip install <package name>` if you want additional packages
-# 5.) `atom .` will open current directory in atom
-# 6.) modify and run code
-# 7.) `deactivate` will exit virtual environment
+
